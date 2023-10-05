cf250ac54c5cfdee5dfc23d7bd499b6ab49d6fff
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-05 20:00:10 +0200

    6

--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-function hello ({name}) {
-  return <h1>Hello, {name}</h1>
-}
-
-//Se la variabile name non viene passata alla funzione il suo valore sarà undefined
-//se la name variabile contiene un'espressione JSX viene renderizzato direttamente nel risultato finale
