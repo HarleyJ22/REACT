f21d6524cb34d97ec69cb1701b8bce6a59b14f2b
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-05 19:22:05 +0200

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,7 @@
-const sum = (a, b) => a + b;
-const result = <div>{sum(22, 22)}</div>
+function hello ({name}) {
+  return <h1>Hello, {name}</h1>
+}
+
+//Se la variabile name non viene passata alla funzione il suo valore sarà undefined
+
+//Se la variabile name constiene un'espressione JSX invece di una stringa, il contenuto verrà renderizzato nel risultato finale
