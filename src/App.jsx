25b1c528758c8c34d04a70372745a4e93a467cbb
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-06 19:37:23 +0200

    29

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 //import { Hello } from "./Hello";
 //import { Welcome } from "./Welcome";
-import { AlertClock } from "./AlertClock";
+//import { AlertClock } from "./AlertClock";
+import { useState } from "react";
 
 function App() {
-    function handleButtonClick() {
-        const now = new Date();
-        alert(`The current time is ${now.toLocaleTimeString()}`);
-    }
+  // function handleButtonClick() {
+  //     const now = new Date();
+  //     alert(`The current time is ${now.toLocaleTimeString()}`);
+  // }
+  const [count, setCount] = useState(0);
+  if (count == 0) {
+    return (
+      <>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </>
+    );
+  }
 
   return (
     <div>
       {/* <Hello /> */}
       {/* <Welcome name={<strong>Giulia</strong>} /> */}
-      <AlertClock buttonClick={handleButtonClick}/>
+      {/* <AlertClock buttonClick={handleButtonClick}/> */}
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is <h2>{count}</h2>
+      </button>
     </div>
   );
 }
