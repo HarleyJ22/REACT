c78b159c9862d0b4c1cfdb43844666cdf362e8c4
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-06 11:39:25 +0200

    13

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Hello } from "./Hello"
+
+function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  )
+}
+export default App
+
+//sì, è possibiele usare il componente Hello più volte perché crea delle istanze indipendenti tra loro 
