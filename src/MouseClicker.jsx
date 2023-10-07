547ce1084d4f701f1e8b4da38c4e1af3009a2ece
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-07 12:59:59 +0200

    38

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -2,13 +2,16 @@ export function MouseClicker({ name }) {
   function clickButton(event) {
     console.log(event.target.name);
   }
+  function imageClick(event) {
+    console.log(event.target.src)
+  }
 
   return (
     <div>
       <button name={name} onClick={clickButton}>
         Event
       </button>
-      <img name="img" width={20} height={20}/>
+      <img onClick={imageClick} name="img" src="https://e7.pngegg.com/pngimages/510/25/png-clipart-blue-power-button-computer-icons-desktop-blue-power-button-icon-miscellaneous-trademark-thumbnail.png" width={50} height={60}/>
     </div>
   );
 }
