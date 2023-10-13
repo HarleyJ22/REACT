9b60ebfa9dd4af4175457fd0cf75e42e66109095
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-13 17:07:33 +0200

    49

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -23,4 +23,11 @@ function UncontrolledLogin() {
 
 export default UncontrolledLogin
 
-//L'API standard è l'API DOM, esistono API non standard ma sono sconsigliate per via dell'incompatibilità trai browser  
+//L'API standard è l'API DOM, esistono API non standard ma sono sconsigliate per via dell'incompatibilità trai browser.
+
+//I vantaggi del FormData sono ad esempio:
+//la sua semplicità d'utilizzo, si può fare direttamente con HTML,
+//supporta il caricamento di file dagli utenti se il modulo lo permette,
+//gestisce la codifica dei dati inviati al server rendendoli compatibili
+//Svantaggi: è progettato per l'invio di dati con richieste HTTP,
+//non è sempre compatibile con vecchie versioni di Explorer ad esempio.
