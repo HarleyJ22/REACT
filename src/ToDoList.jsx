fcc7f5de5b7f4f1a6221f26664971e77915ed01e
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-14 11:49:13 +0200

    56

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,22 +2,22 @@ import { useRef } from "react";
 import { useState } from "react";
 
 function ToDoList() {
-    const [items, setItems] = useState([]);
-    const inputRef = useRef(null);
-    function handleButton() {
-        setItems([...items, inputRef.current.value]);
-    }
-    return (
-        <div>
-        <ul>
-            {items.map((id, item) =>(
-            <li key= {id}>{item}</li>
+  const [items, setItems] = useState([]);
+  const inputRef = useRef(null);
+  function handleButton() {
+    setItems([...items, inputRef.current.value]);
+  }
+  return (
+    <div>
+      <ul>
+        {items.map((id, item) => (
+          <li key={id}>{item}</li>
         ))}
-        </ul>
-        <imput ref={inputRef} type="text"/>
-    <button onClick={handleButton}>Add value</button>
-        </div>
-    )
+      </ul>
+      <imput ref={inputRef} type="text" />
+      <button onClick={handleButton}>Add value</button>
+    </div>
+  );
 }
 
-export default ToDoList
+export default ToDoList;
