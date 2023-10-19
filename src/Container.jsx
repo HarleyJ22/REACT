7d67510c836f23a9428bfa630741dec3df9aa043
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-19 21:06:16 +0200

    68

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,10 +1,10 @@
 export function Container({title, children}) {
     return ( 
         <>
-        {title}
+        <div>{title}</div>
         <div style={{border: "2px solid red", backgroundColor: "white", borderRadius: "10px"}}>
             {children}
-        </div>
+        </div> 
         </>
     )
 }
