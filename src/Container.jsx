a848485d9557bbd13b072674133209559812eb6b
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-19 21:07:34 +0200

    69

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
+
 export function Container({title, children}) {
+    const [collapsed, setCollapsed] = useState(false)
+
+    function hanldeToggleCollapse() {
+        setCollapsed(t => !t)
+    }
     return ( 
         <>
-        <div>{title}</div>
-        <div style={{border: "2px solid red", backgroundColor: "white", borderRadius: "10px"}}>
+        <div>{title} <button onClick={hanldeToggleCollapse}>Toggle</button></div>
+        {collapsed && <div style={{border: "2px solid red", backgroundColor: "white", borderRadius: "10px"}}>
             {children}
-        </div> 
+        </div> }
         </>
     )
 }
