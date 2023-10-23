2ac7caf26c0d41cf2ac5fbe64152281292b4bbb4
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-23 12:50:43 +0200

    23

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,12 +1,12 @@
 import Age from "./Age";
 
 function Welcome({ name, age }) {
-    return (
-        <>
-          <p> Welcome, {name}!</p>
-          {18 < age < 65 ? <Age age={age} /> : "L'età non rientra nei parametri richiesti" }
-        </>
-    )
+  return (
+    <>
+      <p> Welcome, {name}!</p>
+      {age > 18 && age < 65 && <Age age={age} />}
+    </>
+  );
 }
 
 export default Welcome;
