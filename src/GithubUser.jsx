0d17bf52fd1b4a89e0393667009f9ed5f1f9b1d9
Miriana Falcone <miriana.falcone7@gmail.com>
2023-10-26 12:26:32 +0200

    77

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -17,34 +17,3 @@ const GithubUser = ({ username }) => {
 };
 
 export default GithubUser;
-// import { useEffect, useState } from "react";
-
-// export function GithubUser({ username }) {
-//     const [data, setData] = useState(null);
-//     const [error, setError] = useState(null);
-
-//     const fetchData = async() => {
-//         try {
-//             const response = await fetch(`https://api.github.com/users/`);
-//             if (response.ok) {
-//                 const data = await response.json();
-//                 setData(data);
-//             } else {
-//                 setError("error")
-//             }
-//         } catch(error) {
-//             error(error);
-//         }
-//     }
-//     useEffect(() => {
-//         fetchData(username);
-//     }, [username]);
-
-//     return (
-//         <div>
-//             {data &&<h1>{data.login}</h1>}
-//             {data &&<h1>{data.username}</h1>}
-//             {data &&<h2>{data.avatar_url}</h2>}
-//         </div>
-//     )
-// }
