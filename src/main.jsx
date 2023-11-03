5c5b8c6b36789d83aa8e5218ec8bca99f054e92c
Miriana Falcone <miriana.falcone7@gmail.com>
2023-11-03 17:12:17 +0100

    82

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import React from "react";
 import ReactDOM from "react-dom/client";
-import Root from "./Root";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
