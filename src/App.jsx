330123c3b267f2fd4b06e6abbac7a4c13a575ffa
Miriana Falcone <miriana.falcone7@gmail.com>
2023-11-03 17:23:50 +0100

    83

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Welcome from "./Welcome";
+import Counter from "./Counter";
 
 function App() {
   return (
     <div>
         <Routes>
           <Route path="/" element={<Welcome name="Giulia" />} />
+          <Route path="counter" element={<Counter initialValue={0} addOne={1} decrementCount={1}/>} />
         </Routes>
     </div>
   );
