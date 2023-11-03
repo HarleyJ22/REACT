68cd5ef3911088725ad6dc7fdfe531839e8e3607
Miriana Falcone <miriana.falcone7@gmail.com>
2023-11-03 17:54:11 +0100

    84

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Welcome from "./Welcome";
 import Counter from "./Counter";
+import { ShowGithubUser } from "./ShowGitubUser";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Welcome name="Giulia" />} />
           <Route path="counter" element={<Counter initialValue={0} addOne={1} decrementCount={1}/>} />
+          <Route path="/users/:username" element={<ShowGithubUser/>}/>
         </Routes>
     </div>
   );
