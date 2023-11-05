79e3ca9be2fdf79dec446c797d2d01edd9788e06
Miriana Falcone <miriana.falcone7@gmail.com>
2023-11-05 18:06:06 +0100

    85

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import Welcome from "./Welcome";
 import Counter from "./Counter";
 import { ShowGithubUser } from "./ShowGitubUser";
@@ -7,6 +7,19 @@ import { ShowGithubUser } from "./ShowGitubUser";
 function App() {
   return (
     <div>
+      <nav>
+       <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+        <li>
+          <Link to="/users/HarleyJ22">Miriana Falcone</Link>
+        </li>
+        </ul> 
+      </nav>
         <Routes>
           <Route path="/" element={<Welcome name="Giulia" />} />
           <Route path="counter" element={<Counter initialValue={0} addOne={1} decrementCount={1}/>} />
