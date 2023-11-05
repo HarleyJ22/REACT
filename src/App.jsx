e44dcb391884d3b14bfa14fc18691c81a2ae8df7
Miriana Falcone <miriana.falcone7@gmail.com>
2023-11-05 18:18:21 +0100

    86

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,39 @@ import Welcome from "./Welcome";
 import Counter from "./Counter";
 import { ShowGithubUser } from "./ShowGitubUser";
 
-function App() {
+const NotFound = () => (
+  <div>
+    <h1>Error 404</h1>
+  </div>
+);
+
+const App = () => {
   return (
     <div>
       <nav>
-       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/counter">Counter</Link>
-        </li>
-        <li>
-          <Link to="/users/HarleyJ22">Miriana Falcone</Link>
-        </li>
-        </ul> 
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/counter">Counter</Link>
+          </li>
+          <li>
+            <Link to="/users/HarleyJ22">Miriana Falcone</Link>
+          </li>
+        </ul>
       </nav>
-        <Routes>
-          <Route path="/" element={<Welcome name="Giulia" />} />
-          <Route path="counter" element={<Counter initialValue={0} addOne={1} decrementCount={1}/>} />
-          <Route path="/users/:username" element={<ShowGithubUser/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Welcome name="Giulia" />} />
+        <Route
+          path="counter"
+          element={<Counter initialValue={0} addOne={1} decrementCount={1} />}
+        />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
